8ffa176d4baafd90acc051caf5e1cf63686970e0
Dean Attali <daattali@gmail.com>
2020-04-20 03:06:02 -0400

    Updates

---
+++ precache-manifest.82541e524893adbb9981d3f637972fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea1cced31aa0b16775c8a7a32ddbd24",
+    "url": "/settlers-catan-turn-analyzer/index.html"
+  },
+  {
+    "revision": "2f8c593d0b2a9371e61a",
+    "url": "/settlers-catan-turn-analyzer/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "70b86932529e84f4b7ca",
+    "url": "/settlers-catan-turn-analyzer/static/css/main.b0c3e20d.chunk.css"
+  },
+  {
+    "revision": "2f8c593d0b2a9371e61a",
+    "url": "/settlers-catan-turn-analyzer/static/js/2.74b715ea.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/settlers-catan-turn-analyzer/static/js/2.74b715ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b86932529e84f4b7ca",
+    "url": "/settlers-catan-turn-analyzer/static/js/main.5fde9373.chunk.js"
+  },
+  {
+    "revision": "9a5624969778881d0db8",
+    "url": "/settlers-catan-turn-analyzer/static/js/runtime-main.b0b92ef8.js"
+  }
+]);

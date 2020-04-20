9eeeba6b32092a9fe815965c13e7733f10928285
Dean Attali <daattali@gmail.com>
2020-04-20 15:03:57 -0400

    add github link

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,24 +1,15 @@
 import React from 'react'
 
-const Details = ({turns, players}) => {
-  const parsedTurns = turns.map(({player, time, roll}) => {
-    let turn = {player, time, roll}
-    turn.player = players[player]
-    return turn
-  })
-
-  return (
-    <div>
-    {turns.map(turn =>
-      <div key={turn.id}>
-        {turn.id}. {' '}
-        <strong>{players[turn.player]}</strong> {' '}
-        took {turn.time} seconds {' '}
-        {turn.roll === undefined ? null : `(rolled ${turn.roll})`}
-      </div>
-    )}
+const Details = ({turns, players}) =>
+  <div>
+  {turns.map(turn =>
+    <div key={turn.id}>
+      {turn.id}. {' '}
+      <strong>{players[turn.player]}</strong> {' '}
+      took {turn.time} seconds {' '}
+      {turn.roll === undefined ? null : `(rolled ${turn.roll})`}
     </div>
-  )
-}
+  )}
+  </div>
 
 export default Details
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,17 @@ const Header = () =>
   <div>
     <h1>Settlers of Catan Turn Analyzer</h1>
     <div>
-    You <strong>really</strong> think that 4 came up more than the 8?
-    Tired of everyone "wrongly" claiming you take the longest turns? 
-    Only one way to find out!
+      You <strong>really</strong> think that 4 came up more than the 8? {' '}
+      Tired of everyone "wrongly" claiming you take the longest turns? {' '}
+      <strong>Only one way to find out!</strong>
     </div>
-    <em><a href="https://deanattali.com" target="_blank" rel="noopener noreferrer">Dean Attali</a> &middot; April 2020</em>
+    <em>
+      By <a href="https://deanattali.com" target="_blank" rel="noopener noreferrer">Dean Attali</a> {' '}
+      &middot; {' '}
+      April 19, 2020 {' '}
+      &middot; {' '}
+      Code <a href="https://github.com/daattali/settlers-catan-turn-analyzer" target="_blank" rel="noopener noreferrer">on GitHub</a>
+    </em>
     <hr />
   </div>
 
